766abeb119895767a4969a3a6b260259ce3f4802
nely de faria <nelly.nf54@gmail.com>
2025-09-01 12:49:16 -0300

    Create challenge-02.py

---
+++ src/challenge-02.py
@@ -0,0 +1,6 @@
+dayOfBirth = int(input("What is your date of birth? "))
+monthOfBirth = input("What month were you born in? ")
+yearOfBirth = int(input("What year were you born? "))
+
+print("====== CHALLENGE 02 ======")
+print(f"You were born on {dayOfBirth}, {monthOfBirth}, {yearOfBirth}. Is that correct? ")
