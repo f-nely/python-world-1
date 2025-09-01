dc58ba53fc34e9528ca28017a1fa31b29ea74f2f
nely de faria <nelly.nf54@gmail.com>
2025-09-01 14:51:23 -0300

    Create primitive-types.py

---
+++ src/primitive-types.py
@@ -0,0 +1,10 @@
+# Examples of Primitive Data Types
+n1 = 10                    # Integer
+n2 = 10.5                  # Float
+isActive = True            # Bool
+role = "Mobile developer"  # String
+
+print(type(n1))
+print(type(n2))
+print(type(isActive))
+print(type(role))
