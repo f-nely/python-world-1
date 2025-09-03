064f07811e02ed109825b50584e0149ac8c71a19
nely de faria <nelly.nf54@gmail.com>
2025-09-03 15:47:59 -0300

    Create challenge-05.py

---
+++ src/challenge-05.py
@@ -0,0 +1,9 @@
+"""
+Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor
+"""
+
+num = int(input("Enter a number: "))
+
+print(f"Analyzing the value {num}, its predecessor is {num-1} and its successor is {num+1}.")
+
+
