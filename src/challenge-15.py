220498b9b12582b5404fd3cfe69389cc670b8335
nely de faria <nelly.nf54@gmail.com>
2025-09-06 12:23:08 -0300

    Create challenge-15.py

---
+++ src/challenge-15.py
@@ -0,0 +1,9 @@
+"""
+Escreva um programa que pergunte a quantidade de Km percorridos por um carro alugado e a quantidade de dias pelos quais ele foi alugado. Calcule o preço a pagar, sabendo que o carro custa R$60 por dia e R$0.15 por Km rodado
+"""
+
+days = int(input("How many days was the car rented for? "))
+km = float(input("How many kilometers did the car travel? "))
+total = (days * 60) + (km * 0.15)
+
+print(f"The total amount to be paid is R${(total):.2f}")
