97f2750b284f94df278c97d35723ac65db9bf53c
nely de faria <nelly.nf54@gmail.com>
2025-09-06 12:22:46 -0300

    Create challenge-13.py

---
+++ src/challenge-13.py
@@ -0,0 +1,4 @@
+salary = float(input("What is the employee's salary? "))
+newSalary = salary + (salary * 0.15)
+#4,319.43 = 4,967.34
+print(f"The employee used to earn R${salary}, but with a 15% increase, he/she will now earn R${(newSalary):.2f}")
