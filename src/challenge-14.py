620262ede219eacce88b5c22d96f029f33cfb449
nely de faria <nelly.nf54@gmail.com>
2025-09-06 12:22:57 -0300

    Create challenge-14.py

---
+++ src/challenge-14.py
@@ -0,0 +1,7 @@
+"""
+Escreva um programa que converta uma temperatura digitada em Celsius para Fahrenheit
+"""
+
+celsius = float(input("Enter the temperature in °C: "))
+fah = celsius * 1.8 + 32
+print(f"The temperature of {celsius}°C is equal to {fah}°F!")
