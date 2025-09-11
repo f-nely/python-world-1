e56704a59af02c2ccf05782a5d9df13b7d2b22ac
nely de faria <nelly.nf54@gmail.com>
2025-09-11 13:56:06 -0300

    Create modules.py

---
+++ src/modules.py
@@ -0,0 +1,8 @@
+import random
+from math import sqrt
+
+number1 = int(input("Enter a number: "))
+print(f"The square root of {number1} is {sqrt(number1)}!")
+
+number2 = random.randint(1, 10)
+print(number2)
