06fbc3996da2c402c56a384603bd024c26a46355
nely de faria <nelly.nf54@gmail.com>
2025-09-15 15:47:12 -0300

    Create challenge-20.py

---
+++ src/challenge-20.py
@@ -0,0 +1,16 @@
+"""
+Um professor quer sortear um dos seus quatro alunos para apagar o quadro. Faça
+um programa que ajude ele, lendo o nome deles e escrevendo o nome do escolhido
+Haaland
+Donnarumma
+Marmoush
+Trafford
+Cherki
+"""
+from random import choice
+students = list()
+for i in range(0, 4):
+    name = input("Enter the student's name: ")
+    students.append(name)
+
+print(f"The student chosen was {choice(students)}")
