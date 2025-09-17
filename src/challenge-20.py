23635133e2906b94581edcf389125c86da104173
nely de faria <nelly.nf54@gmail.com>
2025-09-17 13:19:50 -0300

    Update comment

--- src/challenge-20.py
+++ src/challenge-20.py
@@ -1,11 +1,6 @@
 """
 Um professor quer sortear um dos seus quatro alunos para apagar o quadro. Faça
 um programa que ajude ele, lendo o nome deles e escrevendo o nome do escolhido
-Haaland
-Donnarumma
-Marmoush
-Trafford
-Cherki
 """
 from random import choice
 students = list()
