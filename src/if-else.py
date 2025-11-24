17c9502638f222fb130c3f54825a1ba9bd508938
nely de faria <nelly.nf54@gmail.com>
2025-11-24 15:51:43 -0300

    Create if-else.py

---
+++ src/if-else.py
@@ -0,0 +1,19 @@
+""" ageCar = int(input("How old is your car: "))
+
+print("New car" if ageCar<=3 else "Old car")
+
+if ageCar <= 3:
+  print("Your car is new!")
+else:
+  print("Your car is old!")
+
+print("--The End--") """
+
+firstName = str(input("What is your name? ")).capitalize()
+
+if firstName == "Gustavo":
+  print("What beautiful name!")
+else:
+  print("Your name is ordinary.")
+
+print(f"Good morning, {firstName}!")
